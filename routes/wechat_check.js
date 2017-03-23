853aebc262f38749b4a6aa241fcd3596ed00754e
yangttlxl@sina.com <yangttlxl@sina.com>
2017-03-23 14:49:35 +0800

    test pull

--- routes/wechat_check.js
+++ routes/wechat_check.js
@@ -5,6 +5,7 @@ var WechatAPI = require('wechat-api');
 
 module.exports = function (ctx, next) {
     var api = new WechatAPI("wx3a3f138d198893c4", "95bdcfaa2bd98f2b9c1bf294570252bd");
+    //ss
 //     api.updateRemark('open_id', 'remarked', function (err, data, res) {
 //   // TODO
 //    });
