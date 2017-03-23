6ea9c3e3650242b24f3a90ffe739755c4d0068bf
yangttlxl@sina.com <yangttlxl@sina.com>
2017-03-23 15:56:19 +0800

    增加微信接入的入口

--- routes/wechat_check.js
+++ routes/wechat_check.js
@@ -1,13 +1,47 @@
+var wechat = require('wechat');
 var WechatAPI = require('wechat-api');
 // var api = new WechatAPI(appid, appsecret);
-
-
-
-module.exports = function (ctx, next) {
-    var api = new WechatAPI("wx3a3f138d198893c4", "95bdcfaa2bd98f2b9c1bf294570252bd");
-    //ss
-//     api.updateRemark('open_id', 'remarked', function (err, data, res) {
-//   // TODO
-//    });
-//   return ctx.render('index', { title: '程序测试' });
+var config = {
+  token: 'weixin',
+  appid: 'wx30b4d102680fa0c2',
+  encodingAESKey: 'COlVMNkJocnZRQ64AszKHuyLfS7M7wxI6nRh2Iz19GI',
+  checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
 };
+
+
+module.exports = wechat(config,function (ctx, next) {
+    // 微信输入信息都在req.weixin上
+  var message = req.weixin;
+  if (message.FromUserName === 'diaosi') {
+    // 回复屌丝(普通回复)
+    res.reply('hehe');
+  } else if (message.FromUserName === 'text') {
+    //你也可以这样回复text类型的信息
+    res.reply({
+      content: 'text object',
+      type: 'text'
+    });
+  } else if (message.FromUserName === 'hehe') {
+    // 回复一段音乐
+    res.reply({
+      type: "music",
+      content: {
+        title: "来段音乐吧",
+        description: "一无所有",
+        musicUrl: "http://mp3.com/xx.mp3",
+        hqMusicUrl: "http://mp3.com/xx.mp3",
+        thumbMediaId: "thisThumbMediaId"
+      }
+    });
+  } else {
+    // 回复高富帅(图文回复)
+    res.reply([
+      {
+        title: '你来我家接我吧',
+        description: '这是女神与高富帅之间的对话',
+        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+        url: 'http://nodeapi.cloudfoundry.com/'
+      }
+    ]);
+  }
+});
